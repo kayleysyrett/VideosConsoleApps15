7229a31910a2b6c3799195f903e08e60a9de4037
kayleysyrett <73078305+kayleysyrett@users.noreply.github.com>
2021-02-03 18:08:00 +0000

    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,52 @@
 ﻿namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This app converts the distance 
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Kayley Syrett version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        
+        private double feet;
 
+        /// <summary>
+        /// This class will run the program
+        /// </summary>
+        public void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Prompt the user to enter the distance
+        /// Input the miles as a double number 
+        /// </summary>
+        private void InputMiles()
+        {
+
+        }
+
+        /// <summary>
+        /// This class offers methods for converting a given 
+        /// distance measured in miles to the equivalent
+        /// distance measured in feet.
+        /// </summary>
+        private void CalculateFeet()
+        {
+
+        }
+
+        /// <summary>
+        /// This class will send the output to the screen
+        /// in feet 
+        /// </summary>
+        private void OutputFeet()
+        {
+
+        }
     }
+
 }
