8bd346f1e3fea0b060e250b32bd88cd5baacfbcd
kayleysyrett <73078305+kayleysyrett@users.noreply.github.com>
2021-03-02 12:55:48 +0000

    added enum BMIunit class

---
+++ ConsoleAppProject/App02/BMIUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App02
+{
+    public enum BMIUnit
+    {
+        Kilograms,
+        Pounds,
+        Stones, 
+        Metres,
+        Inches,
+        Centimetres,
+    }
+}
