04091d3c055ea7c4ef2e350ee59e846adf956234
kayleysyrett <73078305+kayleysyrett@users.noreply.github.com>
2021-03-02 12:59:33 +0000

    added unit class

---
+++ ConsoleAppProject/App02/UnitClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App02
+{
+    public enum UnitClass
+    {
+        metric,
+        imperial,
+    }
+}
