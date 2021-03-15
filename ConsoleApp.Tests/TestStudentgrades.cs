4ded6152714f95adab268b0c32599a692c49846d
kayleysyrett <73078305+kayleysyrett@users.noreply.github.com>
2021-03-15 16:46:32 +0000

    added test methods to StudentGrades.Test

---
+++ ConsoleApp.Tests/TestStudentgrades.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppProject.App03;
+
+namespace ConsoleApp.Tests
+{
+    [TestClass]
+    public class TestStudentGrades
+    {
+        private readonly StudentGrades converter = newStudentGrades(); 
+
+        [TestMethod]
+        public void TestConvert0ToGradeF()
+        {
+            // Arrange 
+
+            Grades actualGrade = converter.ConvertToGrade(0);
+
+            //Assert
+
+            Assert.AreEqual(expectedGrade, actualgrade);
+        }
+    }
+}
