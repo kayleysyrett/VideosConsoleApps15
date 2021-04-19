33f4fcd10628ff1c202e943d19239a337315d3f4
kayleysyrett <73078305+kayleysyrett@users.noreply.github.com>
2021-04-19 15:06:43 +0100

    Added a base class, and ised inheritance  for the posts

--- ConsoleAppProject/App04/NewsFeed.cs
+++ ConsoleAppProject/App04/NewsFeed.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-
 
 namespace ConsoleAppProject.App04
 {
@@ -20,16 +18,14 @@ namespace ConsoleAppProject.App04
     ///</author> 
     public class NewsFeed
     {
-        private readonly List<MessagePost> messages;
-        private readonly List<PhotoPost> photos;
+        private readonly List<Post> posts;
 
         ///<summary>
         /// Construct an empty news feed.
         ///</summary>
         public NewsFeed()
         {
-            messages = new List<MessagePost>();
-            photos = new List<PhotoPost>();
+            posts = new List<Post>();
         }
 
 
@@ -40,7 +36,7 @@ namespace ConsoleAppProject.App04
         ///</summary>
         public void AddMessagePost(MessagePost message)
         {
-            messages.Add(message);
+            posts.Add(message);
         }
 
         ///<summary>
@@ -50,7 +46,7 @@ namespace ConsoleAppProject.App04
         ///</summary>
         public void AddPhotoPost(PhotoPost photo)
         {
-            photos.Add(photo);
+            posts.Add(photo);
         }
 
         ///<summary>
@@ -60,16 +56,9 @@ namespace ConsoleAppProject.App04
         public void Display()
         {
             // display all text posts
-            foreach (MessagePost message in messages)
+            foreach (Post post in posts)
             {
-                message.Display();
-                Console.WriteLine();   // empty line between posts
-            }
-
-            // display all photos
-            foreach (PhotoPost photo in photos)
-            {
-                photo.Display();
+                posts.Display();
                 Console.WriteLine();   // empty line between posts
             }
         }
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -11,7 +11,7 @@ namespace WebApp
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build()
+            var host = CreateHostBuilder(args).Build();
             CreateDbIfNotExists(host);
 
             host.Run();
